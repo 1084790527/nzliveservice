b059e41358fb34b810390e928fd784e70e2834ad
1084790527@qq.c <1084790527@qq.com>
2018-10-09 11:27:49 +0800

    INIT

--- src/main/java/com/example/nzliveservice/controller/LoginController.java
+++ src/main/java/com/example/nzliveservice/controller/LoginController.java
@@ -1,4 +1,20 @@
 package com.example.nzliveservice.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.example.nzliveservice.bean.UserLogin;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class LoginController {
+
+    @RequestMapping(value = "login")
+    @ResponseBody
+    private String login(@RequestBody UserLogin userLogin){
+        System.out.println(userLogin.toString());
+
+        return "asdasd";
+    }
 }
