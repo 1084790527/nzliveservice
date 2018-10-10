1f5e35a9536d544ce812ab74e4dcd7c90ce303c5
1084790527@qq.c <1084790527@qq.com>
2018-10-10 17:20:44 +0800

    INIT

--- src/main/java/com/example/nzliveservice/controller/LoginController.java
+++ src/main/java/com/example/nzliveservice/controller/LoginController.java
@@ -32,27 +32,46 @@ public class LoginController {
 //        System.out.println("AAA:"+student.getUserid().length());
         if (loginUser.getUserid().length()==6){
             Teacher user=userLoginDao.getUserTeacherLogin(loginUser.getUserid());
-        }
-        Student user=userLoginDao.getUserStudentLogin(loginUser.getUserid());
-        if (user==null){
-            //账号不存在
-            jsonObject.put("status","0");
-            return  jsonObject;
-        }else if (!user.getUserpwd().equals(loginUser.getUserpwd())){
-            //密码错误
-            jsonObject.put("status","1");
-            return  jsonObject;
+            if (user==null){
+                //账号不存在
+                jsonObject.put("status","0");
+                return  jsonObject;
+            }else if (!user.getUserpwd().equals(loginUser.getUserpwd())){
+                //密码错误
+                jsonObject.put("status","1");
+                return  jsonObject;
+            }else {
+                //账号密码正确
+                jsonObject.put("status","2");
+                jsonObject.put("userid",user.getUserid());
+                jsonObject.put("username",user.getUsername());
+                jsonObject.put("userpwd",user.getUserpwd());
+                jsonObject.put("system",user.getSystem());
+                System.out.println(loginUser.toString());
+                System.out.println(""+user.toString());
+                return jsonObject;
+            }
         }else {
-            //账号密码正确
-            jsonObject.put("status","2");
-            jsonObject.put("userid",user.getUserid());
-            jsonObject.put("dormroom",user.getDormroom());
-            jsonObject.put("username",user.getUsername());
-            jsonObject.put("userpwd",user.getUserpwd());
-            System.out.println(loginUser.toString());
-            System.out.println(""+user.toString());
-            return jsonObject;
+            Student user=userLoginDao.getUserStudentLogin(loginUser.getUserid());
+            if (user==null){
+                //账号不存在
+                jsonObject.put("status","0");
+                return  jsonObject;
+            }else if (!user.getUserpwd().equals(loginUser.getUserpwd())){
+                //密码错误
+                jsonObject.put("status","1");
+                return  jsonObject;
+            }else {
+                //账号密码正确
+                jsonObject.put("status","2");
+                jsonObject.put("userid",user.getUserid());
+                jsonObject.put("dormroom",user.getDormroom());
+                jsonObject.put("username",user.getUsername());
+                jsonObject.put("userpwd",user.getUserpwd());
+                System.out.println(loginUser.toString());
+                System.out.println(""+user.toString());
+                return jsonObject;
+            }
         }
-
     }
 }
