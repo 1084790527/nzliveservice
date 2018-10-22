64bbfcc127acc0864d37869178e70f97e280e5b0
1084790527 <1084790527@qq.com>
2018-10-22 10:04:43 +0800

    INIT

--- src/main/java/com/example/nzliveservice/NzliveserviceApplication.java
+++ src/main/java/com/example/nzliveservice/NzliveserviceApplication.java
@@ -1,10 +1,14 @@
 package com.example.nzliveservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 
+
 @SpringBootApplication
+@EnableAutoConfiguration
 public class NzliveserviceApplication {
 
 	public static void main(String[] args) {
